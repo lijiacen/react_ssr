3cd34e248a398a6f43856770ffbff7413001c8e4
lijiacen <50915968+lijiacen@users.noreply.github.com>
2020-07-19 17:42:53 +0800

    抽离公共webpack文件，项目目录优化

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,33 +1,12 @@
 const path = require("path");
+const { merge } = require("webpack-merge");
+const baseConfig = require("./webpack.base");
 
-module.exports = {
+module.exports = merge(baseConfig, {
   mode: "development",
   entry: "./src/client/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            "@babel/react",
-            [
-              "@babel/env",
-              {
-                targets: {
-                  browsers: ["last 2 versions"]
-                }
-              }
-            ]
-          ],
-          plugins: ["@babel/plugin-proposal-function-bind"]
-        }
-      }
-    ]
   }
-};
+});
--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const nodeExternals = require("webpack-node-externals"); //部分后端用的node包被babel-loader的node_modules忽略,需要用此工具导入
+const { merge } = require("webpack-merge");
+const baseConfig = require("./webpack.base");
 
-module.exports = {
+module.exports = merge(baseConfig, {
   target: "node",
   mode: "development",
   entry: "./src/index.js",
@@ -9,28 +11,5 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build")
   },
-  externals: [nodeExternals()],
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            "@babel/react",
-            [
-              "@babel/env",
-              {
-                targets: {
-                  browsers: ["last 2 versions"]
-                }
-              }
-            ]
-          ],
-          plugins: ["@babel/plugin-proposal-function-bind"]
-        }
-      }
-    ]
-  }
-};
+  externals: [nodeExternals()]
+});
